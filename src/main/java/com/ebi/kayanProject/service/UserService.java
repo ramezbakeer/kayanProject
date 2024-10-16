496275e440da9af07a88464427771714031427a9
ramez bakeer <77814416+ramezbakeer@users.noreply.github.com>
2024-10-16 15:46:03 +0300

    Update UserService.java

--- src/main/java/com/ebi/kayanProject/service/UserService.java
+++ src/main/java/com/ebi/kayanProject/service/UserService.java
@@ -24,73 +24,11 @@ public class UserService implements UserServiceInt {
 
     @Override
     public UserDto RegisterUser(UserDto userDto) {
-
-
-//            if(userDto.getFName()==null || userDto.getFName()==""){
-//                return null;
-//            }
-//            if(userDto.getLName()==null || userDto.getLName()==""){
-//                return null;
-//            }
-//            if(userDto.getEmail()==null || userDto.getEmail()==""){
-//                return null;
-//            }
-//            if(userDto.getPassword()==null || userDto.getPassword()==""){
-//                return null;
-//            }
-//            if(userDto.getPhone()==null || userDto.getPhone()==""){
-//                return null;
-//            }
-//            if(userDto.getAddress()==null || userDto.getAddress()==""){
-//                return null;
-//            }
-//            if(userDto.getJobTitle()==null || userDto.getJobTitle()==""){
-//                return null;
-//            }
             userDto.setPassword(encoder.encode(userDto.getPassword()));
             return modelMapper.map((userInt.save(modelMapper.map(userDto, UserEntity.class))), UserDto.class);
 
     }
 
-//    @Override
-//    public LoginUserDto LoginUser(LoginUserDto loginUserDto) {
-//        UserEntity userEntity = userInt.findByFName(loginUserDto.getFName());
-//            if (userEntity != null && userEntity.getPassword().equals(loginUserDto.getPassword())) {
-//                return modelMapper.map(userEntity, LoginUserDto.class);
-//            } else {
-//                throw new CustomException("Wrong password Or FName Invalid","/logIn");
-//            }
-//    }
-
-
-
-
-
-
-//    public UserDto getUserDto(UserEntity userEntity) {
-//        UserDto userDto = new UserDto();
-//        userDto.setId(userEntity.getId());
-//        userDto.setPhone(userEntity.getPhone());
-//        userDto.setEmail(userEntity.getEmail());
-//        userDto.setPassword(userEntity.getPassword());
-//        userDto.setAddress(userEntity.getAddress());
-//        userDto.setFName(userEntity.getFName());
-//        userDto.setLName(userEntity.getLName());
-//        userDto.setJobTitle(userEntity.getJobTitle());
-//        return userDto;
-//    }
-//    public UserEntity getUserEntity(UserDto userDto) {
-//        UserEntity userEntity = new UserEntity();
-//        userEntity.setId(userDto.getId());
-//        userEntity.setPhone(userDto.getPhone());
-//        userEntity.setEmail(userDto.getEmail());
-//        userEntity.setPassword(userDto.getPassword());
-//        userEntity.setFName(userDto.getFName());
-//        userEntity.setLName(userDto.getLName());
-//        userEntity.setAddress(userDto.getAddress());
-//        userEntity.setJobTitle(userDto.getJobTitle());
-//        return userEntity;
-//    }
     public LoginUserDto getLoginUserDto(UserEntity userEntity) {
         LoginUserDto loginUserDto = new LoginUserDto();
         loginUserDto.setFName(userEntity.getFName());
