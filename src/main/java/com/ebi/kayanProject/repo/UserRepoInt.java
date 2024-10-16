3f6191c0e555f161c8c2dd8ab2b48af0de9a6f78
ramez bakeer <77814416+ramezbakeer@users.noreply.github.com>
2024-10-16 15:43:31 +0300

    Update UserRepoInt.java

--- src/main/java/com/ebi/kayanProject/repo/UserRepoInt.java
+++ src/main/java/com/ebi/kayanProject/repo/UserRepoInt.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepoInt extends JpaRepository<UserEntity, Long> {
-//    @Query("from UserEntity e where e.email = :email")
-//    public  UserEntity findByEmail(String email);
     @Query("from UserEntity e where e.fName = :firstName")
     public UserEntity findByFName(String firstName);
 }
